7a1344e976b18e87efff06bd4680291f9b8b5dfe
Jordan Callahan <jordan.s.callahan@gmail.com>
2017-10-15 12:29:49 +0000

    automatically backed up by learn

--- arrays.js
+++
@@ -1,7 +0,0 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray(array, element)  {
-  return array = [element,...array]
-}
-
-function destructivelyAddElementToBeginningOfArray
