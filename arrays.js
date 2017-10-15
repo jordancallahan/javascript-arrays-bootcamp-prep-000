bf159811c4cb3236b17b303bc5520eac04d28eb4
Jordan Callahan <jordan.s.callahan@gmail.com>
2017-10-15 12:43:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,8 @@ function addElementToEndOfArray(array, element)  {
 }
 
 function destructivelyAddElementToEndOfArray(array, element)  {
-  return array = [array.push(element)]
+  if (array.push(element))
+  return array;
 }
 
 function accessElementInArray(array, element)  {
