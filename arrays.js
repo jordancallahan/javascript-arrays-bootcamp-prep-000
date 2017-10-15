73f9cc5975d6a59af5d48f1d35cc03031c134eca
Jordan Callahan <jordan.s.callahan@gmail.com>
2017-10-15 12:53:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,5 @@ function destructivelyRemoveElementFromEndOfArray(array)  {
 }
 
 function removeElementFromEndOfArray(array)  {
-  if (array.slice(0, array.length - 1))
-  return array;
+  return array.slice(0, array.length - 1))
 }
